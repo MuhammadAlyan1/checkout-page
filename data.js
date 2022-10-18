dd723da38f21c4166a8961bde493a1c00ca01614
Muhammad Alyan <alyan0332@gmail.com>
2022-10-18 17:16:55 +0500

    add data

---
+++ data.js
@@ -0,0 +1,17 @@
+import vintageBackpack from './public/photo1.png';
+import leviShoes from './public/photo2.png';
+
+export const cartData = [
+  {
+    image: vintageBackpack,
+    product: 'Vintage Backpack',
+    originalPrice: 94.99,
+    discountPrice: 54.99,
+  },
+  {
+    image: leviShoes,
+    product: 'Levi Shoes',
+    originalPrice: 124.99,
+    discountPrice: 74.99,
+  },
+];
